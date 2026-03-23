ef45e7974575902a1d47a825153df4a84809a79f
itouyuuki <itou@nextreservation.jp>
2026-03-23 17:08:13 +0900

    fix

---
+++ mekulo/parameters.php
@@ -0,0 +1,59 @@
+<?php
+$SPECTRUM_PARAMS = [
+ 
+    '顧客獲得スタイル' => [
+        'work_location' => [
+            'labelname'    => '外回り度',
+            'left'   => 'オフィス中心',
+            'right'  => '外回り中心',
+        ],
+        'approach' => [
+            'labelname'    => '既存顧客比率',
+            'left'   => '新規（狩猟）',
+            'right'  => '既存（農耕）',
+        ],
+        'customer_type' => [
+            'labelname'    => '法人顧客比率',
+            'left'   => '個人顧客',
+            'right'  => '法人顧客',
+        ],
+        'new_existing' => [
+            'labelname'    => 'ファーミング度',
+            'left'   => '新規開拓',
+            'right'  => 'ファーミング',
+        ],
+        'inbound_outbound' => [
+            'labelname'    => 'インバウンド度',
+            'left'   => 'アウトバウンド',
+            'right'  => 'インバウンド',
+        ],
+    ],
+ 
+    '営業難易度' => [
+        'price' => [
+            'labelname'    => '商材単価',
+            'left'   => '低単価',
+            'right'  => '高単価',
+        ],
+        'sales_cycle' => [
+            'labelname'    => '営業サイクル',
+            'left'   => '即決型',
+            'right'  => '長期提案',
+        ],
+        'proposal_depth' => [
+            'labelname'    => '提案深度',
+            'left'   => '御用聞き型',
+            'right'  => '課題提案型',
+        ],
+        'product_type' => [
+            'labelname'    => '無形商材度',
+            'left'   => '有形商材',
+            'right'  => '無形商材',
+        ],
+        'price_authority' => [
+            'labelname'    => '価格裁量',
+            'left'   => '定価販売',
+            'right'  => '価格交渉可能',
+        ],
+    ],
+];
